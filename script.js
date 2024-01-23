7d74dcee1e704c7d59eabae94f44bb23bd8fd2b2
Ashton <ashton.decker@stu.barren.kyschools.us>
2024-01-23 09:46:58 -0600

    Exercise complete

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(6+1);
+// Numbers, Strings, Boolean, Null, Undefined
+// +, -, *, /, **, %
+console.log(5 % 4);
+console.log(5 % 2);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Ashton" .length + "Decker" .length);
+console.log("Avengers: Endgame" [16]);
+
+
